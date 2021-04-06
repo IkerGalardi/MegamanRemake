245eaf15f034254edc61021ce7c511f81ea56481
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-04-06 13:56:28 +0200

    No dependency between framerate and physics

--- src/game/player/player_system.cc
+++ src/game/player/player_system.cc
@@ -9,40 +9,47 @@ player_system::player_system(std::shared_ptr<spdlog::logger> logger)
 }
 
 void player_system::on_update(float dtime) {
-    auto& registry = get_scene()->get_registry();
-    auto view = registry.view<engine::transform_component, player_component>();
-    for(auto entity : view) {
-        auto [transform, rigid, player] = registry.get<engine::transform_component, 
-                                                       engine::rigidbody, 
-                                                       player_component>(entity);
+    static float acum = 0.0f;
+    acum += dtime;
 
-        b2Vec2 movement{0.0f, 0.0f};
-        float scale = transform.scale.x;
-        if(engine::application::get().keyboard().key_state(SDLK_a)) {
-            // Move the player
-            movement.x = -player.movement_speed * dtime;
-            
-            // Flip the player if it needs to be fliped
-            if(scale > 0) {
-                transform.scale.x = -scale;
-            }
-            rigid->ApplyForceToCenter(20 * movement, true);
-        } 
-        if(engine::application::get().keyboard().key_state(SDLK_d)) {
-            // Move the player
-            movement.x = player.movement_speed * dtime;
+    if(acum > 0.033333333f/2.0f) {
+        acum = 0.0f;
 
-            // Flip the player if it needs to be fliped
-            if(scale < 0) {
-                transform.scale.x = -scale;
+        auto& registry = get_scene()->get_registry();
+        auto view = registry.view<engine::transform_component, player_component>();
+        for(auto entity : view) {
+            auto [transform, rigid, player] = registry.get<engine::transform_component, 
+                                                           engine::rigidbody, 
+                                                           player_component>(entity);
+
+            b2Vec2 movement{0.0f, 0.0f};
+            float scale = transform.scale.x;
+            if(engine::application::get().keyboard().key_state(SDLK_a)) {
+                // Move the player
+                movement.x = -player.movement_speed * dtime;
+
+                // Flip the player if it needs to be fliped
+                if(scale > 0) {
+                    transform.scale.x = -scale;
+                }
+                rigid->ApplyForceToCenter(300 * movement, true);
+            } 
+            if(engine::application::get().keyboard().key_state(SDLK_d)) {
+                // Move the player
+                movement.x = player.movement_speed * dtime;
+
+                // Flip the player if it needs to be fliped
+                if(scale < 0) {
+                    transform.scale.x = -scale;
+                }
+                rigid->ApplyForceToCenter(300 * movement, true);
             }
-            rigid->ApplyForceToCenter(20 * movement, true);
-        }
-        
-        if(engine::application::get().keyboard().key_state(SDLK_SPACE)) {
-            rigid->ApplyLinearImpulseToCenter(b2Vec2{0.0, 0.0003}, true);
-        } else {
-            rigid->ApplyLinearImpulseToCenter(b2Vec2_zero, true);
+
+            if(engine::application::get().keyboard().key_state(SDLK_SPACE)) {
+                rigid->ApplyLinearImpulseToCenter(b2Vec2{0.0, .05f}, true);
+            } else {
+                rigid->ApplyLinearImpulseToCenter(b2Vec2_zero, true);
+            }
         }
     }
 }
